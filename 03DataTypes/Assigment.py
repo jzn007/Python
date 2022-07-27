de9e6842516fd3683d15bb87f2ad7121bd2c6042
Juan Zepeda Nolasco <jzn007@hotmail.com>
2022-07-27 18:35:47 -0500

    DataTypes

---
+++ 03DataTypes/Assigment.py
@@ -0,0 +1,25 @@
+firstName = "Juan"
+lastName = "Zepeda Nolasco"
+age = 43
+ssn = "ZENJ010122GN9"
+height = 1.70
+weight = 80.0
+
+print(firstName)
+print(type(firstName))
+
+print(lastName)
+print(type(lastName))
+
+print(age)
+print(type(age))
+
+print(ssn)
+print(type(ssn))
+
+print(height)
+print(type(height))
+
+print(weight)
+print(type(weight))
+
---
+++ Comments.py
@@ -0,0 +1,20 @@
+""" multiple 
+lines """
+#Comments single line
+from tkinter import Y
+
+
+print("Python is easy!")
+
+#indentation
+x = int(input("Enter min number"))
+y = int(input("Enter max number"))
+
+i = x
+
+if i % 2 == 0 : i = x +1
+while i<=y:
+    print(i)
+    i+=2
+
+
