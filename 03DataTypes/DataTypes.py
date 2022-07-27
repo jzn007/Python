6db0428a2daf52aef54249cc224acdd32e085eef
Juan Zepeda Nolasco <jzn007@hotmail.com>
2022-07-27 18:31:46 -0500

    DataTypes

---
+++ 03DataTypes/DataTypes.py
@@ -0,0 +1,48 @@
+#Integer
+a = 13
+b = 100
+c = -66
+
+print(a,b,c)
+print(type(a)) #To know what type is
+
+#Floating
+x = 33.5
+y = -22.8
+z = 205.0
+
+print (x,y,z)
+print(type(z))
+
+#Complex types
+d = 3 + 5j
+print(d)
+print(type(d))
+
+#Binary
+e = 0B1010
+print(e) #binary value
+print(type(e))
+
+#Hexdecimal
+f = 0XFF 
+print(f)
+print(type(f))
+
+g = True
+print(g)
+print(type(g))
+print(9<8)
+
+#Convert Functions
+h = int(x)
+print(h)
+print(type(h))
+
+i = float("22.5")
+print(i)
+print(type(i))
+
+print(bin(10))
+print(oct(10))
+
